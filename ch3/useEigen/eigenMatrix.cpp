63064eaa7f730adb092835e1f4465990fb023219
liyubo <liyubo_liyubo@163.com>
2017-12-15 20:25:50 +0800

    add homework in ch3

--- ch3/useEigen/eigenMatrix.cpp
+++ ch3/useEigen/eigenMatrix.cpp
@@ -6,7 +6,9 @@ using namespace std;
 // 稠密矩阵的代数运算（逆，特征值等）
 #include <Eigen/Dense>
 
+#include "hw.h"
 #define MATRIX_SIZE 50
+
 
 /****************************
 * 本程序演示了 Eigen 基本类型的使用
@@ -106,29 +108,8 @@ int main( int argc, char** argv )
 //    x = matrix_NN.colPivHouseholderQr().solve(v_Nd);
 //    cout <<"time use in Qr decomposition is " <<1000*  (clock() - time_stt)/(double)CLOCKS_PER_SEC <<"ms" << endl;
 
-    //作业
-    Eigen::MatrixXd matrix_A;
-    matrix_A = Eigen::MatrixXd::Random( 100, 100 );
-    Eigen::MatrixXd matrix_b;
-    matrix_b = Eigen::MatrixXd::Random( 100, 1 );
-    Eigen::MatrixXd x;
-   // cout << matrix_A << endl;
-   // cout << matrix_b << endl;
 
-    clock_t time_stt = clock();
-    x = matrix_A.colPivHouseholderQr().solve(matrix_b);  //利用QR分解求解方程
-    cout <<"time use in Qr decomposition is " <<1000*  (clock() - time_stt)/(double)CLOCKS_PER_SEC <<"ms" << endl;
-
-
-    time_stt = clock();
-    x = matrix_A.inverse()*matrix_b;;  //利用求逆来解方程
-    cout <<"time use  in normal inverse  is " <<1000*  (clock() - time_stt)/(double)CLOCKS_PER_SEC <<"ms" << endl;
-
-
-    time_stt = clock();
-    x = matrix_A.llt().solve(matrix_b);   //Cholesky来解方程
-    cout <<"time use  in Cholesky  is " <<1000*  (clock() - time_stt)/(double)CLOCKS_PER_SEC <<"ms" << endl;
-
+    homework();
     return 0;
 }
 
