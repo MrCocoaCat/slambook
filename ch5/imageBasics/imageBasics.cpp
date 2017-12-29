9bf7c6fac8f2214ae9949675c552a971a02ea47e
liyubo <liyubo_liyubo@163.com>
2017-12-29 12:02:26 +0800

    @liyubo

--- ch5/imageBasics/imageBasics.cpp
+++ ch5/imageBasics/imageBasics.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <chrono>
-using namespace std;
-
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
 
 int main ( int argc, char** argv )
 {
